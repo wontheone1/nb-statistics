8c5cbcbc3ca120da4286691c988eb76ba6bb526c
WON SEOB SEO <won.seob.seo@zalando.fi>
2017-09-17 17:42:44 +0300

    Implement wholesale analysis

--- src/nb_statistics/core.clj
+++ src/nb_statistics/core.clj
@@ -6,6 +6,9 @@
 (def ^:private retail-available-colors
   [#"회색" #"하늘" #"검정" #"핑크" #"백색" #"혼합"])
 
+(def ^:private whole-sale-available-colors
+  [#"OR" #"PK" #"DE" #"NY" #"BU" #"GY" #"AS"])
+
 (defn greater-than-and-less-than [min max num]
   (and num (> num min) (> max num)))
 
@@ -24,23 +27,38 @@
 (defn- extract-column-from-rows [col-index rows]
   (map (fn [row] (row col-index)) rows))
 
-(defn- extract-size-and-color-pairs [options]
+(defn- extract-size-from-string [string]
+  (some->> string
+           (re-seq #"\d+")
+           (map remove-leading-0s)
+           (map clojure.edn/read-string)
+           (filter (fn [num] (greater-than-and-less-than 60 130 num)))
+           last))
+
+(defn- extract-color-from-string [string available-color-patterns default]
+  (when string
+    (or
+      (some (fn [color]
+              (-> (re-seq color string)
+                  last))
+            available-color-patterns)
+      default)))
+
+(defn- extract-size-and-color-pairs-from-options [options]
   (map
     (fn [option]
-      (let [size (some->> option
-                          (re-seq #"\d+")
-                          (map remove-leading-0s)
-                          (map clojure.edn/read-string)
-                          (filter (fn [num] (greater-than-and-less-than 60 130 num)))
-                          first)
-            color (when option
-                    (or
-                      (some (fn [color]
-                              (re-find color option))
-                            retail-available-colors)
-                      "빈칸"))]
+      (let [size (extract-size-from-string option)
+            color (extract-color-from-string option retail-available-colors "빈칸")]
         [size color]))
     options))
+
+(defn- extract-size-and-color-pairs-from-product-names [product-names]
+  (map
+    (fn [product-name]
+      (let [size (extract-size-from-string product-name)
+            color (extract-color-from-string product-name whole-sale-available-colors "빈칸")]
+        [size color]))
+    product-names))
 
 (defn- add-size-and-color-columns-to-row [row]
   (concat row ["사이즈" "색상"]))
@@ -58,11 +76,11 @@
                    result)))
 
 (defn run-retail-mode!
-  [file-name]
+  [filename]
   (let [{:keys [first-row contents-rows]} (separate-first-row-and-contents-rows
-                                            (read-files-as-row-vector file-name))
+                                            (read-files-as-row-vector filename))
         options (extract-column-from-rows 4 contents-rows)
-        size-color-pairs (extract-size-and-color-pairs options)
+        size-color-pairs (extract-size-and-color-pairs-from-options options)
         first-row-with-size-and-color (add-size-and-color-columns-to-row first-row)
         contents-rows-with-size-and-color (add-columns-to-rows contents-rows size-color-pairs)]
     (write-results-to-csv-file "retail-output.csv"
@@ -70,7 +88,15 @@
                                      contents-rows-with-size-and-color))))
 
 (defn run-wholesale-mode! [filename]
-  (println (str filename " not implemented")))
+  (let [{:keys [first-row contents-rows]} (separate-first-row-and-contents-rows
+                                            (read-files-as-row-vector filename))
+        product-names (extract-column-from-rows 2 contents-rows)
+        size-color-pairs (extract-size-and-color-pairs-from-product-names product-names)
+        first-row-with-size-and-color (add-size-and-color-columns-to-row first-row)
+        contents-rows-with-size-and-color (add-columns-to-rows contents-rows size-color-pairs)]
+    (write-results-to-csv-file "whole-output.csv"
+                               (cons first-row-with-size-and-color
+                                     contents-rows-with-size-and-color))))
 
 (defn -main
   [& args]
